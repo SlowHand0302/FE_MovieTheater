7481e8e9e708e958586c337e9f88d1a6eb9f471c
Nguyen Nhat Thong <work.nguyennhatthong@gmail.com>
2025-11-05 18:55:24 +0700

    refactor(interface): update enum for cinema and seat interface

--- src/interfaces/Cinema.interface.ts
+++ src/interfaces/Cinema.interface.ts
@@ -1,11 +1,18 @@
 import { Auditable } from './Auditable.interface';
 
+export enum CinemaStatus {
+    ACTIVE = 'active',
+    INACTIVE = 'inactive',
+    MAINTENANCE = 'maintenance',
+    CLOSED = 'closed',
+}
+
 export interface Cinema extends Auditable {
     name: string;
     address: string;
     phoneNumber: string;
     email: string;
-    status: string;
+    status: CinemaStatus;
     open_Time: string;
     close_Time: string;
     city: string;
--- src/interfaces/Seat.interface.ts
+++ src/interfaces/Seat.interface.ts
@@ -1,12 +1,18 @@
 import { Auditable } from './Auditable.interface';
 
+export enum SeatStatus {
+    AVAILABLE = 'available',
+    BOOKED = 'booked',
+    RESERVED = 'reserved',
+}
+
 export interface Seat extends Auditable {
     label: string;
     columnIndex: number;
     displayNumber: number;
     seatCode: string;
     isActive: boolean;
-    status: string;
+    status: SeatStatus;
     seatTypeId: string;
     roomId: string;
 }
