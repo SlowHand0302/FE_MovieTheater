0b8b30f83adf308719951d6f13f9cab770e700e8
Nguyen Nhat Thong <work.nguyennhatthong@gmail.com>
2025-12-06 19:12:09 +0700

    refactor(component): refactor ui of client header component

--- src/app/(client)/components/Header.tsx
+++ src/app/(client)/components/Header.tsx
@@ -2,8 +2,9 @@
 
 import * as React from 'react';
 import Link from 'next/link';
+import { GalleryVerticalEnd, Search } from 'lucide-react';
 
-import { cn } from '@/lib/utils';
+import { useIsMobile } from '@/hooks/use-mobile';
 import {
     NavigationMenu,
     NavigationMenuContent,
@@ -14,123 +15,89 @@ import {
     navigationMenuTriggerStyle,
 } from '@/components/ui/navigation-menu';
 import { Command, CommandInput } from '@/components/ui/command';
-import { GalleryVerticalEnd, Search } from 'lucide-react';
-import Account from '@/components/AccountDropdown';
-import SearchBar from '@/components/Searchbar';
-import Overlay from '@/components/Overlay';
 import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import Account from '@/components/AccountDropdown';
 
 export function Header() {
-    const [openSearchBar, setOpenSearchBar] = React.useState<boolean>(false);
+    const isMobile = useIsMobile();
 
     return (
-        <>
-            <NavigationMenu className="justify-between fixed top-0 left-1/2 -translate-x-1/2 z-10 backdrop-blur-sm p-3 max-w-full w-[1850px] 2xl:mx-auto">
-                <NavigationMenuList>
-                    <NavigationMenuItem>
-                        <NavigationMenuLink
-                            asChild
-                            className={`${navigationMenuTriggerStyle()} flex items-center justify-center hover:bg-transparent`}
-                        >
-                            <Link href="/" passHref className="bg-transparent">
-                                <div className="flex justify-center gap-2 md:justify-start">
-                                    <div className="flex items-center gap-2 font-medium">
-                                        <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-                                            <GalleryVerticalEnd className="size-4 text-white" />
-                                        </div>
-                                        Cine Inc.
+        <NavigationMenu
+            viewport={isMobile}
+            className="justify-between shadow-sm fixed top-0 left-1/2 -translate-x-1/2 z-10 backdrop-blur-sm p-3 max-w-full w-[1850px] 2xl:mx-auto"
+        >
+            <NavigationMenuList className="flex-wrap">
+                <NavigationMenuItem>
+                    <NavigationMenuLink
+                        asChild
+                        className={`${navigationMenuTriggerStyle()} flex items-center justify-center hover:bg-transparent`}
+                    >
+                        <Link href="/" passHref className="bg-transparent">
+                            <div className="flex justify-center gap-2 md:justify-start">
+                                <div className="flex items-center gap-2 font-medium">
+                                    <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+                                        <GalleryVerticalEnd className="size-4 text-white" />
                                     </div>
+                                    Cine Inc.
                                 </div>
-                            </Link>
-                        </NavigationMenuLink>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem className="md:block hidden">
-                        <NavigationMenuTrigger className="bg-transparent hover:bg-transparent focus:bg-transparent">
-                            Danh Sách Phim
-                        </NavigationMenuTrigger>
-                        <NavigationMenuContent>
-                            <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                                <ListItem href="/docs" title="Phim Đang Chiếu">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate facere deleniti
-                                    rerum, laborum hic veritatis nesciunt eum ratione necessitatibus enim, molestias
-                                    inventore ipsa placeat, iste quod qui ut in temporibus?{' '}
-                                </ListItem>
-                                <ListItem href="/docs/installation" title="Phim Sắp Chiếu">
-                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et, est? Officia quia
-                                    perferendis voluptatibus quae, harum sed maiores nemo laborum molestias error unde
-                                    maxime vero neque ex inventore tempora veniam.{' '}
-                                </ListItem>
-                            </ul>
-                        </NavigationMenuContent>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem className="md:block hidden">
-                        <NavigationMenuLink
-                            asChild
-                            className={`${navigationMenuTriggerStyle()} hover:bg-transparent focus:bg-transparent`}
-                        >
-                            <Link href="/docs" passHref className="bg-transparent">
-                                Sự kiện và Tin Tức
-                            </Link>
-                        </NavigationMenuLink>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem className="md:block hidden">
-                        <NavigationMenuLink
-                            asChild
-                            className={`${navigationMenuTriggerStyle()} hover:bg-transparent focus:bg-transparent`}
-                        >
-                            <Link href="/docs" passHref className="bg-transparent">
-                                Rạp và Giá Vé
-                            </Link>
-                        </NavigationMenuLink>
-                    </NavigationMenuItem>
-                </NavigationMenuList>
-                <NavigationMenuList className="items-center h-full flex cursor-pointer">
-                    <NavigationMenuItem onClick={() => setOpenSearchBar(true)}>
-                        <Command className="rounded-lg border bg-accent/50 border-none hidden md:block xl:min-w-[450px]">
-                            <CommandInput
-                                placeholder="Type a command or search..."
-                                className="cursor-pointer"
-                                readOnly
-                            />
-                        </Command>
-                        <Button variant="ghost" className="block md:hidden">
-                            <Search className="w-4 h-4" />
-                        </Button>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <Account />
-                    </NavigationMenuItem>
-                </NavigationMenuList>
-            </NavigationMenu>
-            {openSearchBar && (
-                <Overlay classname="flex justify-center items-start pt-40" onClick={() => setOpenSearchBar(false)}>
-                    <SearchBar />
-                </Overlay>
-            )}
-        </>
+                            </div>
+                        </Link>
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
+                <NavigationMenuItem className="hidden md:block">
+                    <NavigationMenuTrigger className="bg-transparent hover:bg-transparent focus:bg-transparent">
+                        Danh sách phim
+                    </NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                        <ul className="grid w-[200px] gap-4">
+                            <li>
+                                <NavigationMenuLink asChild>
+                                    <Link href="/movie">Phim đang chiếu</Link>
+                                </NavigationMenuLink>
+                                <NavigationMenuLink asChild>
+                                    <Link href="/movie">Phim sắp chiếu</Link>
+                                </NavigationMenuLink>
+                            </li>
+                        </ul>
+                    </NavigationMenuContent>
+                </NavigationMenuItem>
+                <NavigationMenuItem className="hidden md:block">
+                    <NavigationMenuLink
+                        asChild
+                        className={cn(
+                            navigationMenuTriggerStyle(),
+                            'bg-transparent hover:bg-transparent focus:bg-transparent',
+                        )}
+                    >
+                        <Link href="/movie">Sự kiện và Tin Tức</Link>
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
+                <NavigationMenuItem className="hidden md:block">
+                    <NavigationMenuLink
+                        asChild
+                        className={cn(
+                            navigationMenuTriggerStyle(),
+                            'bg-transparent hover:bg-transparent focus:bg-transparent',
+                        )}
+                    >
+                        <Link href="/movie">Rạp và Giá Vé</Link>
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
+            </NavigationMenuList>
+            <NavigationMenuList className="items-center h-full flex cursor-pointer">
+                <NavigationMenuItem>
+                    <Command className="rounded-lg border bg-accent/50 border-none hidden md:block xl:min-w-[450px]">
+                        <CommandInput placeholder="Type a command or search..." className="cursor-pointer" readOnly />
+                    </Command>
+                    <Button variant="ghost" className="block md:hidden">
+                        <Search className="w-4 h-4" />
+                    </Button>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <Account />
+                </NavigationMenuItem>
+            </NavigationMenuList>
+        </NavigationMenu>
     );
 }
-
-const ListItem = React.forwardRef<React.ElementRef<'a'>, React.ComponentPropsWithoutRef<'a'>>(
-    ({ className, title, children, ...props }, ref) => {
-        return (
-            <li>
-                <NavigationMenuLink asChild>
-                    <a
-                        ref={ref}
-                        className={cn(
-                            'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
-                            className,
-                        )}
-                        {...props}
-                    >
-                        <div className="text-sm font-medium leading-none">{title}</div>
-                        <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">{children}</p>
-                    </a>
-                </NavigationMenuLink>
-            </li>
-        );
-    },
-);
-
-ListItem.displayName = 'ListItem';
