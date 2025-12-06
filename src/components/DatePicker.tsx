24cb902061c7b229ef99ccb8d12d898958a04115
Nguyen Nhat Thong <work.nguyennhatthong@gmail.com>
2025-12-06 19:14:15 +0700

    feat(component): add date picker component

---
+++ src/components/DatePicker.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import * as React from 'react';
+import { ChevronDownIcon } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
+import { Calendar } from '@/components/ui/calendar';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+
+interface DatePickerProps {
+    selectedDate: Date;
+    setSelectedDate: (date: Date | undefined) => void;
+}
+
+export default function DatePicker({ selectedDate, setSelectedDate }: DatePickerProps) {
+    const [open, setOpen] = React.useState(false);
+
+    return (
+        <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+                <Button variant="outline" id="date" className="w-48 justify-between font-normal">
+                    {selectedDate ? selectedDate.toLocaleDateString() : 'Select date'}
+                    <ChevronDownIcon />
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto overflow-hidden p-0" align="start">
+                <Calendar
+                    mode="single"
+                    selected={selectedDate}
+                    captionLayout="dropdown"
+                    onSelect={(date) => {
+                        setSelectedDate(date);
+                        setOpen(false);
+                    }}
+                />
+            </PopoverContent>
+        </Popover>
+    );
+}
