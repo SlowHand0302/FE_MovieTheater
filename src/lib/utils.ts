85ac28aa33487be516569c2cba38135fd707068f
Nguyen Nhat Thong <work.nguyennhatthong@gmail.com>
2025-12-08 18:53:42 +0700

    feat(util): add decode jwt and verify jwt expired function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,38 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import { jwtDecode, type JwtPayload, type JwtHeader } from 'jwt-decode';
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
+}
+
+export function decodeJWT(token: string) {
+    if (!token) {
+        return null;
+    }
+
+    try {
+        const header = jwtDecode<JwtHeader>(token, { header: true });
+        const payload = jwtDecode<JwtPayload>(token);
+        return { header, payload };
+    } catch (error) {
+        console.log(error);
+        return { header: null, payload: null };
+    }
+}
+
+export function isJwtExpired(token: string) {
+    if (!token) {
+        return true;
+    }
+    try {
+        const payload = jwtDecode(token);
+        const currentTime = Date.now() / 1000;
+        if (!payload.exp) return false;
+
+        return payload.exp && payload.exp <= currentTime;
+    } catch (error) {
+        console.log(error);
+        return true;
+    }
 }
